d2ca15451f1e1a5cf73366c6c758f49630b51abf
lfs <luciano2023silva@gmail.com>
2022-06-21 21:29:42 -0300

    feat: structure for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
-import { gql, useQuery } from '@apollo/client';
-import { useEffect } from 'react'
-import { client } from './lib/apollo'
+import { Event } from './pages/Event';
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-
-    teachers {
-      id
-      name
-    }
-  }
-`;
-
-
-interface Lesson {
-  id: string;
-  title: string;
-}
 
 function App() {
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
-
-  console.log(data)
-
   return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <Event />
   )
 }
 
