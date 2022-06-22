92ea26a4a285096cb5e92e93e36f065a5095c304
lfs <luciano2023silva@gmail.com>
2022-06-21 21:22:22 -0300

    task: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+      colors: {
+        green: {
+          300: '#00b37e',
+          500: '#00875f',
+          700: '#015f43'
+        },
+        blue: {
+          500: '#81d8f7'
+        },
+        orange: {
+          500: '#fba94c'
+        },
+        red: {
+          500: '#f75a68'
+        },
+        gray: {
+          100: '#e1e1e6',
+          200: '#c4c4cc',
+          300: '#8d8d99',
+          500: '#323238',
+          600: '#29292e',
+          700: '#121214',
+          900: '#09090a'
+        }
+      }
+    },
   },
   plugins: [],
 }
