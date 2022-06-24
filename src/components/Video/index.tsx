db4ea366a2fc0bec19ee9da753a84424e5ac4c0d
lfs <luciano2023silva@gmail.com>
2022-06-23 21:05:15 -0300

    feat: aspect ratio style

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,7 +1,9 @@
 export function Video() {
   return (
     <div className='flex-1'>
-      Video
+      <div className='bg-black flex justify-center'>
+        <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'></div>
+      </div>
     </div>
   )
 }
