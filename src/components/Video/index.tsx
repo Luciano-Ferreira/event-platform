e501f61009ed28c895c19c4c8374c1ff4ffe8cb2
lfs <luciano2023silva@gmail.com>
2022-06-24 04:05:34 -0300

    feat: avatar section

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -16,6 +16,19 @@ export function Video() {
             <p className='mt-4 text-gray-200 leading-relaxed'>
               Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.
             </p>
+
+            <div className='flex items-center gap-4 mt-6'>
+              <img 
+                className='h-16 w-16 rounded-full border-2 border-blue-500'
+                src="https://github.com/luciano-ferreira.png" 
+                alt="Luciano Silva"
+              />
+
+              <div className='leading-relaxed'>
+                <strong className='font-bold text-2xl block'>Luciano Silva</strong>
+                <span className='text-gray-200 text-sm block'>Front End Developer</span>
+              </div>
+            </div>
           </div>
 
           <div className='flex flex-col gap-4'>
