e0f4789aba4ead61430ccea82d301d07bec319e7
lfs <luciano2023silva@gmail.com>
2022-06-28 13:50:28 -0300

    feat: codegen setup

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -2,50 +2,21 @@ import { Player, Youtube, DefaultUi } from '@vime/react';
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from 'phosphor-react';
 
 import '@vime/core/themes/default.css';
-import { gql, useQuery } from '@apollo/client';
+import { useGetLessonBySlugQuery } from '../../graphql/generated';
 
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-
-query GetLessonBySlug ($slug: String) {
-  lesson(where: {slug: $slug }) {
-    title
-    videoId
-    description
-    teacher {
-      bio
-      avatarURL
-      name
-    }
-  }
-}
-`;
-
-interface GetLessonBySlugResponse { 
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string
-    }
-  }
-}
 interface Props {
   lessonSlug: string;
 }
 
 export function Video({ lessonSlug }: Props) {
 
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
-    variables : {
-      slug: lessonSlug
+  const { data } = useGetLessonBySlugQuery({
+    variables: {
+      slug: lessonSlug,
     }
-  });
+  })
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className='flex-1'>
         <h1>Loading...</h1>
@@ -75,18 +46,22 @@ export function Video({ lessonSlug }: Props) {
               {data?.lesson.description}
             </p>
 
-            <div className='flex items-center gap-4 mt-6'>
-              <img
-                className='h-16 w-16 rounded-full border-2 border-blue-500'
-                src={data?.lesson.teacher.avatarURL}
-                alt={data?.lesson.teacher.name}
-              />
+            {
+              data.lesson.teacher && (
+                <div className='flex items-center gap-4 mt-6'>
+                  <img
+                    className='h-16 w-16 rounded-full border-2 border-blue-500'
+                    src={data?.lesson.teacher.avatarURL}
+                    alt={data?.lesson.teacher.name}
+                  />
 
-              <div className='leading-relaxed'>
-                <strong className='font-bold text-2xl block'>{data?.lesson.teacher.name}</strong>
-                <span className='text-gray-200 text-sm block'>{data?.lesson.teacher.bio}</span>
-              </div>
-            </div>
+                  <div className='leading-relaxed'>
+                    <strong className='font-bold text-2xl block'>{data?.lesson.teacher.name}</strong>
+                    <span className='text-gray-200 text-sm block'>{data?.lesson.teacher.bio}</span>
+                  </div>
+                </div>
+              )
+            }
           </div>
 
           <div className='flex flex-col gap-4'>
@@ -103,7 +78,7 @@ export function Video({ lessonSlug }: Props) {
         </div>
         <div className='gap-8 mt-20 grid grid-cols-2'>
           <a
-            className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'
+            className=' flex  transition-colorsflex w-full xl:w-auto bg-gray-700 rounded overflow-hidden items-stretch md:justify-between gap-6 hover:bg-gray-600 border-2 border-green-500 border-opacity-0 hover:-translate-y-2 hover:border-opacity-100 transitions-all duration-500 ease-in-out'
             href=""
           >
             <div className='bg-green-700 h-full p-6 flex items-center'>
@@ -120,7 +95,7 @@ export function Video({ lessonSlug }: Props) {
             </div>
           </a>
           <a
-            className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'
+            className=' flex  transition-colorsflex w-full xl:w-auto bg-gray-700 rounded overflow-hidden items-stretch md:justify-between gap-6 hover:bg-gray-600 border-2 border-green-500 border-opacity-0 hover:-translate-y-2 hover:border-opacity-100 transitions-all duration-500 ease-in-out'
             href=""
           >
             <div className='bg-green-700 h-full p-6 flex items-center'>
