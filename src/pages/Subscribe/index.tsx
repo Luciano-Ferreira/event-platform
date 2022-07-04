93261345eeecfbae2bb5a0e70508c522b7a086a7
lfs <luciano2023silva@gmail.com>
2022-07-03 23:13:31 -0300

    feat: add toastify

--- src/pages/Subscribe/index.tsx
+++ src/pages/Subscribe/index.tsx
@@ -1,8 +1,11 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { Logo } from '../../components/Logo';
-import { useCreateSubscriberMutation } from '../../graphql/generated';
+import { useCreateSubscriberMutation, usePublishSubscriberMutation } from '../../graphql/generated';
 import bgImage from '/src/assets/code-mockup.png';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Loading } from '../../components/Loading';
 
 type FormValues = {
   name: string;
@@ -11,29 +14,45 @@ type FormValues = {
 
 
 export function Subscribe() {
+
   const navigate = useNavigate();
 
   const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
-  
-  const [ createSubscriber, { loading } ] = useCreateSubscriberMutation()
+
+  const [createSubscriber, { loading, error }] = useCreateSubscriberMutation();
+  const [publishSubscriber, { error: publishError }] = usePublishSubscriberMutation()
+
 
 
   const onSubmit: SubmitHandler<FormValues> = async ({ name, email }) => {
-    try {
-      await createSubscriber({
+
+    const subscriberData = await createSubscriber({
+      variables: {
+        name,
+        email
+      }
+    })
+
+    console.log(subscriberData)
+
+    if (subscriberData) {
+      const publishedData = await publishSubscriber({
         variables: {
-          name,
           email
         }
       })
 
-      navigate('/event')
+      if ((error || publishError) && publishedData) {
+        toast.error(error ? error.message : publishError?.message + " 😮‍💨", {type: "error"})
+      } else {
 
-    } catch (err) {
-      console.error(err)
-
-
+        toast.success('Cadastrado com sucesso! 🚀')
+      }
     }
+
+    
+    navigate('/event')
+
   };
 
 
@@ -59,31 +78,51 @@ export function Subscribe() {
             className='flex flex-col gap-2 w-full'
           >
             <input
-              className='bg-gray-900 rounded px-5 h-14'
+              className='bg-gray-900 rounded px-5 h-14 border-none placeholder-gray-300 focus:outline-0 focus:ring-green-300 focus:border-green-300
+              hover:border-green-300 transition-all duration-500 ease-in-out'
               type="text"
               placeholder='Seu nome completo'
-              {...register('name', { 
+              {...register('name', {
                 required: 'Nome obrigatorio',
+              })}
+            />
+            <p className='text-red-600'>{errors.name?.message}</p>
+
+            <input
+              className='bg-gray-900 rounded px-5 h-14 border-none placeholder-gray-300 focus:outline-0 focus:ring-green-300 focus:border-green-300
+              hover:border-green-300 transition-all duration-500 ease-in-out invalid:text-red-500
+              focus:invalid:border-red-500 focus:invalid:ring-red-500'
+              type="email"
+              placeholder='Digite seu e-mail'
+
+              {...register('email', {
+                required: 'E-mail obrigatorio',
 
               })}
             />
-            <input
-              className='bg-gray-900 rounded px-5 h-14'
-              type="email"
-              placeholder='Digite seu e-mail'
-              {...register('email')}
-            />
+            <p className='text-red-600'>{errors.email?.message}</p>
 
             <button
               type='submit'
-              className='mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors'
+              className='mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:cursor-not-allowed'
               disabled={loading}
             >
-              Garantir minha vaga
+              {loading ? <Loading /> : 'Garantir minha vaga'}
             </button>
           </form>
         </div>
       </div>
+      <ToastContainer
+        position="top-left"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <img src={bgImage} className='mt-10' alt="mockup" />
     </div>
   )
